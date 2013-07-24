25e85f8382920f964b43834e00935e73e7692154
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-07-24 19:03:02 +0300

    RSP in progress

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -6,7 +6,21 @@ def rps_result(m1, m2)
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+
+	values = ["r", "p", "s"]
+	if game.length != 2
+		raise('WrongNumberOfPlayersError')
+	end
+	play = [game[0][1].downcase, game[1][1].downcase]
+	if !(values.include?(play[0])) || !(values.include?(play[1]))
+		raise('NoSuchStrategyError')
+	end
+	winning_plays = [["r","s"], ["s", "p"], ["p", "r"]]
+	if (play[0] == play[1] || winning_plays.include?(play))
+		return game[0]
+	else 
+		return game[1]
+	end
 end
 
 def rps_tournament_winner(tournament)
