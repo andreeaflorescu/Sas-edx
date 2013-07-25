d36b8cd2f395f94c6078cd8e31922364afdfed9b
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-07-25 16:35:21 +0300

    extend existing classes and METAPROGRAMMING

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -1,19 +1,24 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
-  def method_missing(method_id)
-    singular_currency = method_id.to_s.gsub( /s$/, '')
-    if @@currencies.has_key?(singular_currency)
-      self * @@currencies[singular_currency]
-    else
-      super
+    @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0}
+    def method_missing(method, *args)
+        singular_currency = (method == :in ? args.first : method).to_s.gsub(/s$/, '')
+        if @@currencies.has_key?(singular_currency)
+          self.send(method == :in ? :/ : :*, @@currencies[singular_currency])
+        else
+          super
+        end
     end
-  end
 end
 
 class String
-  # YOUR CODE HERE
+    def palindrome?
+        letters = self.downcase.scan(/\w/)
+        letters == letters.reverse
+    end
 end
 
 module Enumerable
-  # YOUR CODE HERE
+    def palindrome?
+        self.to_a == self.to_a.reverse
+    end
 end
