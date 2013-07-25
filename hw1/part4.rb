68172ee625bd810bb690f0a8ab02ed4cf45b248c
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-07-25 11:19:31 +0300

    CODING STYLE

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,36 +1,38 @@
 class Dessert
-  def initialize(name, calories)
-    @name = name
-	@calories = calories
-  end
-  attr_accessor :name, :calories
-  
-  def healthy?
-    if @calories < 200
-		true
-	else
-		false
+	def initialize(name, calories)
+		@name = name
+		@calories = calories
 	end
-  end
-  
-  def delicious?
-    true
-  end
+	attr_accessor :name, :calories
+
+	def healthy?
+		if @calories < 200
+			true
+		else
+			false
+		end
+	end
+
+	def delicious?
+		true
+	end
 end
 
 class JellyBean < Dessert
-  def initialize(name, calories, flavor)
-    @name = name
-	@calories = calories
-	@flavor = flavor
-  end
-  @@not_good =  "black licorice"
-  attr_accessor :flavor
-  def delicious?
-    if @flavor == @@not_good
-		false
-	else
-		super
+	def initialize(name, calories, flavor)
+		@name = name
+		@calories = calories
+		@flavor = flavor
 	end
-  end
+
+	@@not_good =  "black licorice"
+	attr_accessor :flavor
+	
+	def delicious?
+		if @flavor == @@not_good
+			false
+		else
+			super
+		end
+	end
 end
