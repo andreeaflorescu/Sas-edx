ff216984d6e94142e48c13a23f0d77312cc99853
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-07-25 16:33:55 +0300

    Cartesian product of 2 sequences

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,20 @@
+# Given two collections (of possibly different lengths),
+# get the Cartesian product of the sequences. 
+
+require 'forwardable'
+
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+  extend Forwardable
+  @product
+  attr_accessor :product
+
+  def initialize (xCollection, yCollection)
+  	@product = Array.new
+  	xCollection.each {|x| yCollection.each {|y| @product << [x,y]}}
+  end
+
+	def each &block
+		@product.each { |elem| yield elem}
+	end
 end
